3eff309ca74c03dd1507a1d23894aabec3225237
Snow <onyangogeorge954@gmail.com>
2024-05-22 13:34:31 +0300

    chore: setup new firebase app project

--- app/src/main/java/com/nyabwana/kula_area/ReviewsActivity.java
+++ app/src/main/java/com/nyabwana/kula_area/ReviewsActivity.java
@@ -2,6 +2,7 @@ package com.nyabwana.kula_area;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -36,6 +37,13 @@ public class ReviewsActivity extends AppCompatActivity {
 
         restaurantReview = findViewById(R.id.editTextReview);
         reviewSubmit = findViewById(R.id.review_button);
+
+        reviewSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createReview();
+            }
+        });
 
     }
 
